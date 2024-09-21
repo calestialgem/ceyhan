14e454a647c8ede8e9338aa0e307652ab17209bf
Cem GEÇGEL <gecgelcem@outlook.com>
2024-09-21 07:26:43 +0300

    Setup `VERSION`

--- release.bat
+++ release.bat
@@ -1,20 +1,16 @@
 @echo off
 
-for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
-set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
-set "HH=%dt:~8,2%" & set "II=%dt:~10,2%" & set "SS=%dt:~12,2%"
+set /p version=< "src\VERSION"
 
-set version=%YYYY%%MM%%DD%%HH%%II%%SS%
+rmdir /s /q "release\latest"
+mkdir "release\latest"
+xcopy /v /e "res\" "release\latest\res\"
+cmake -DCMAKE_BUILD_TYPE:STRING=Release -Wno-deprecated --no-warn-unused-cli -S "." -B "release\latest\build" -G Ninja
+cmake --build "release\latest\build" --config release --target cey
+copy /v "release\latest\build\cey.exe" "release\latest\cey.exe"
+rmdir /s /q "release\latest\build"
 
-rmdir /s /q "rel\latest"
-mkdir "rel\latest"
-
-cmake -DCMAKE_BUILD_TYPE:STRING=Release -Wno-deprecated --no-warn-unused-cli -S "." -B "rel\latest\build" -G Ninja
-cmake --build "rel\latest\build" --config release --target cey
-copy /v "rel\latest\build\cey.exe" "rel\latest\cey.exe"
-rmdir /s /q "rel\latest\build"
-
-cd "rel\latest"
+cd "release\latest"
 del "..\cey-%version%.zip"
 7z a -tzip "..\cey-%version%.zip" "*"
 cd "..\.."
