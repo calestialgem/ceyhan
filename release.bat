63671cad015dc73fe9640debde3af875f0aa744c
Cem GEÇGEL <gecgelcem@outlook.com>
2024-09-23 11:26:47 +0300

    Fix copying libraries to release

--- release.bat
+++ release.bat
@@ -4,7 +4,7 @@ set /p version=< "src\VERSION"
 
 rmdir /s /q "release\latest"
 mkdir "release\latest"
-xcopy /v /e "res\" "release\latest\res\"
+xcopy /v /e "seyhan\lib\" "release\latest\lib\"
 cmake -DCMAKE_BUILD_TYPE:STRING=Release -Wno-deprecated --no-warn-unused-cli -S "." -B "release\latest\build" -G Ninja
 cmake --build "release\latest\build" --config release --target cey
 copy /v "release\latest\build\cey.exe" "release\latest\cey.exe"
